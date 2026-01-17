7e441f9563ed9f20c0f6eb23cd7d0c5375fdbee0
blokyk <courvoisier.zoe@gmail.com>
2026-01-17 17:06:57 +0000

    chore: simplify pkgs/default.nix expression

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,25 +4,8 @@
 }:
 let
   newpkgs = pkgs.extend (import ../maintainers.nix).overlay;
-  inherit (newpkgs) lib;
 in
-with lib; let
-  # function to get all (non-recursive) sub folders of a given Path
-  listDirectories = dirPath:
-    let
-      dirEntries = attrNames (
-        filterAttrs
-          (entry: kind: kind == "directory")
-          (builtins.readDir dirPath)
-      );
-    in
-      map (path.append dirPath) dirEntries;
-in
-  # create an attr set like { foo = callPackage ./foo {}; bar = callPackage ./bar {}; }
-  # (where `foo` and `bar` are subfolders of this one)
-  genAttrs'
-    (listDirectories ./.)
-    (pkgDir: nameValuePair
-      (toString (baseNameOf pkgDir)) # foo =
-      (newpkgs.callPackage pkgDir {}) # callPackage ./foo {}
-    )
+  newpkgs.lib.packagesFromDirectoryRecursive {
+    callPackage = newpkgs.callPackage;
+    directory = ./.;
+  }
