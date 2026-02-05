be49c745aa829e446bc0bf6c7d9f3066f9cfcb88
blokyk <courvoisier.zoe@gmail.com>
2026-02-05 19:22:02 +0100

    suwayomi: just turn the fucker into a fixed out derivation
    with the `deps.json`-based gradle hook, even just slightly changing the version
    or applying patches means that you have to manually run the update script and
    update the dependency file everyt time. that's so much more work than other
    kinds of derivations (e.g. rust and go just have a single hash to update thanks
    to having all the dependencies in a fixed-output derivation).
    
    it's been two days that i've been trying to write a FOD for the `deps.json`
    file and it's so much worse and harder than i expected, in part the way the
    gradle hook works is by running a partial build, capturing all dependencies,
    and then injecting them back afterwards when doing the proper full build, all
    while managing the `mitm-cache` proxy to inject those locked dependencies into
    the gradle build process.
    
    for my own sanity (and because of the fact i actually use this derivation on my
    own server, and need to update it etc.), i instead just chose to make it the
    whole thing a FOD.
    
    this did require a hack, because:
      1. outputting multiple files and folders requires `outputHashMode: recursive`
      2. we can't use `recursive` because it doesn't allow outputs to contain
         references to the nix store, and our final jar and executable obviously
         contains references to the JRE and other things in the nix store
      3. therefore, we're forced to use `outputHashMode: flat`, but this also means
         that we can only output a single, non-executable file; in our case, that
         file will be the fat jar file generated by the build
      4. however, a jar file isn't directly executable, so we still need to wrap it
         in a tiny bash script that will call the jre with the jar and forward all
         arguments

--- pkgs/suwayomi-server/package.nix
+++ pkgs/suwayomi-server/package.nix
@@ -4,123 +4,121 @@
   jdk,
   jre,
   kotlin,
-  makeWrapper,
   stdenv,
+  writeShellApplication,
 
   enableSystemTray ? false,
 
   lib,
 }@args:
 let
+
   gradle = gradle_9;
   jdk = args.jdk.override { headless = !enableSystemTray; };
   jre = args.jre.override { headless = !enableSystemTray; };
-in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "suwayomi-server";
-  version = "2.1.2061";
-  rev = "a58dcc6f19cf0f1ea27c0042f6e229d7bb3ff8af";
+  suwayomi-unwrapped = stdenv.mkDerivation (finalAttrs: {
+    pname = "suwayomi-server";
+    version = "2.1.2061";
+    rev = "a58dcc6f19cf0f1ea27c0042f6e229d7bb3ff8af";
 
-  src = fetchFromGitHub {
-    owner = "Suwayomi";
-    repo = "Suwayomi-Server";
-    rev = finalAttrs.rev;
-    hash = "sha256-7ckf4TVOfgZQyivNkzi6aWNU/r9DTbGsVaPwmtJr5Vg=";
-  };
+    src = fetchFromGitHub {
+      owner = "Suwayomi";
+      repo = "Suwayomi-Server";
+      rev = finalAttrs.rev;
+      hash = "sha256-7ckf4TVOfgZQyivNkzi6aWNU/r9DTbGsVaPwmtJr5Vg=";
+    };
 
-  patchPhase = ''
-    runHook prePatch
+    outputHashAlgo = "sha256";
+    outputHashMode = "flat";
+    outputHash = "sha256-xk2nOqyjF3IEPlcA8qVQ+529fCJDPRsd+cdrqSmcrFQ=";
 
-    # set the version correctly
-    substituteInPlace buildSrc/src/main/kotlin/Constants.kt \
-      --replace-fail 'v2.1.''${getCommitCount()}' '${finalAttrs.version}'
+    patchPhase = ''
+      runHook prePatch
 
-    # fix the build date to the unix epoch
-    substituteInPlace server/build.gradle.kts \
-      --replace-fail 'Instant.now().epochSecond.toString()' '0'
+      # set the version correctly
+      substituteInPlace buildSrc/src/main/kotlin/Constants.kt \
+        --replace-fail 'v2.1.''${getCommitCount()}' '${finalAttrs.version}'
 
-    runHook postPatch
-  '';
+      # fix the build date to the unix epoch
+      substituteInPlace server/build.gradle.kts \
+        --replace-fail 'Instant.now().epochSecond.toString()' '0'
 
-  # this is required for using mitm-cache on Darwin
-  __darwinAllowLocalNetworking = true;
-  mitmCache = gradle.fetchDeps {
-    pkg = finalAttrs.finalPackage;
-    data = ./deps.json;
-    useBwrap = false;
-  };
+      echo -e '\nkotlin.daemon.jvmargs=-Xmx4G' >> gradle.properties
 
-  # it seems we have to do a full build of the whole app to
-  # get *all* the dependencies (e.g. the AndroidCompat onces)
-  gradleUpdateTask = ":server:shadowJar";
-  # let
-  #   versionOptionals = ver: vals: lib.optionals (lib.versionAtLeast finalAttrs.version ver) vals;
-  #   projects = [
-  #     "buildSrc"
-  #     "AndroidCompat" "AndroidCompat:Config"
-  #     "server"
-  #   ]
-  #   ++ versionOptionals "2.1.1867" [ "server:i18n" ]
-  #   ++ versionOptionals "2.1.1909"
-  #       [ "server:server-config" "server:server-config-generate" ];
-  # in
-  #   [":nixDownloadDeps"] ++ map (project: ":${project}:nixDownloadDeps") projects;
-
-  enableParallelBuilding = true;
-
-  nativeBuildInputs = [
-    gradle
-    jdk
-    kotlin
-    makeWrapper
-    # stripJavaArchivesHook # build's fixup becomes *extremely* slow (couldn't finish on my laptop)
-  ];
-
-  buildInputs = [
-    jre
-  ];
-
-  gradleFlags = [
-    # the kotlin build can fail if there's not enough memory
-    "-Dkotlin.daemon.jvmargs=-Xmx2G"
-  ];
-
-  env = {
-    "ProductBuildType" = "Stable";
-  };
-
-  gradleBuildTask = ":server:shadowJar";
-
-  gradleCheckTask = ":server:test";
-
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm644 ./server/build/Suwayomi-Server-*.jar $out/share/java/tachidesk-server.jar
-
-    makeWrapper ${lib.getExe jre} $out/bin/tachidesk-server \
-      --add-flags "-jar $out/share/java/tachidesk-server.jar"
-
-    runHook postInstall
-  '';
-
-  meta = {
-    description = "Free and open source manga reader server that runs extensions built for Mihon (Tachiyomi)";
-    longDescription = ''
-      Suwayomi is an independent Mihon (Tachiyomi) compatible software and is not a Fork of Mihon (Tachiyomi).
-
-      Suwayomi-Server is as multi-platform as you can get. Any platform that runs java and/or has a modern browser can run it. This includes Windows, Linux, macOS, chrome OS, etc.
+      runHook postPatch
     '';
-    homepage = "https://github.com/Suwayomi/Suwayomi-Server";
-    downloadPage = "https://github.com/Suwayomi/Suwayomi-Server/releases";
-    changelog = "https://github.com/Suwayomi/Suwayomi-Server/releases/tag/v${finalAttrs.version}";
-    license = lib.licenses.mpl20;
-    platforms = jdk.meta.platforms;
-    sourceProvenance = [
-      lib.sourceTypes.fromSource
-      lib.sourceTypes.binaryBytecode
+
+    enableParallelBuilding = true;
+
+    nativeBuildInputs = [
+      jdk
+      kotlin
+      gradle.unwrapped # use unwrapped so it doesn't inject the setup hook
+      # stripJavaArchivesHook # build's fixup becomes *extremely* slow (couldn't finish on my laptop)
     ];
-    maintainers = with lib.maintainers; [ blokyk ];
-    mainProgram = "tachidesk-server";
+
+    buildInputs = [
+      jre
+    ];
+
+    env = {
+      "ProductBuildType" = "Stable";
+    };
+
+    buildPhase = ''
+      runHook preBuild
+
+      gradle --parallel --console plain --no-daemon \
+        :server:shadowJar
+
+      runHook postBuild
+    '';
+
+    checkPhase = ''
+      runHook preCheck
+
+      gradle --parallel --console plain --no-daemon \
+        :server:check
+
+      runHook postCheck
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm644 ./server/build/Suwayomi-Server-*.jar $out
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Free and open source manga reader server that runs extensions built for Mihon (Tachiyomi)";
+      longDescription = ''
+        Suwayomi is an independent Mihon (Tachiyomi) compatible software and is not a Fork of Mihon (Tachiyomi).
+
+        Suwayomi-Server is as multi-platform as you can get. Any platform that runs java and/or has a modern browser can run it. This includes Windows, Linux, macOS, chrome OS, etc.
+      '';
+      homepage = "https://github.com/Suwayomi/Suwayomi-Server";
+      downloadPage = "https://github.com/Suwayomi/Suwayomi-Server/releases";
+      changelog = "https://github.com/Suwayomi/Suwayomi-Server/releases/tag/v${finalAttrs.version}";
+      license = lib.licenses.mpl20;
+      platforms = jdk.meta.platforms;
+      sourceProvenance = [
+        lib.sourceTypes.fromSource
+        lib.sourceTypes.binaryBytecode
+      ];
+      maintainers = with lib.maintainers; [ blokyk ];
+      mainProgram = "tachidesk-server";
+    };
+  });
+in
+writeShellApplication {
+  name = "tachidesk-server";
+  text = ''
+    ${lib.getExe jre} -jar ${suwayomi-unwrapped} "''${@}"
+  '';
+
+  derivationArgs = {
+    inherit (suwayomi-unwrapped) meta;
   };
-})
+}
