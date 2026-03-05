e1294a46f965ae071e07ea78551c4ed1f0c85e7c
blokyk <courvoisier.zoe@gmail.com>
2026-03-05 02:50:45 +0100

    nit(pkgs): exclude weird values from zpkgs attrset

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,9 +3,15 @@
   ...
 }:
 let
+  inherit (pkgs.lib) filterAttrs isDerivation packagesFromDirectoryRecursive;
+
   newpkgs = pkgs.extend (import ../maintainers.nix).overlay;
+  zpkgs = packagesFromDirectoryRecursive {
+    inherit (newpkgs) callPackage newScope;
+    directory = ./.;
+  };
 in
-newpkgs.lib.packagesFromDirectoryRecursive {
-  inherit (newpkgs) callPackage newScope;
-  directory = ./.;
-}
+  # only keep attrs that are derivations, since we don't use namespaces
+  # (also exclude the `default` attr immediately, which is this
+  # expression, which would cause infinite recursion)
+  filterAttrs (name: val: name != "default" && isDerivation val) zpkgs
