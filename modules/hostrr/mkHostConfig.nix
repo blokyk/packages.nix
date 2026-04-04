d0cfe69243ba18854006e99d2ca4b3a2035d2b6e
blokyk <courvoisier.zoe@gmail.com>
2026-04-04 16:07:31 +0200

    hostrr: merge location configs for ports and links correctly

--- modules/hostrr/mkHostConfig.nix
+++ modules/hostrr/mkHostConfig.nix
@@ -1,14 +1,25 @@
 { lib, ... }:
+cfg:
 with lib;
 let
   mkLinkLocation = name: config: {
     name = "= /" + name;
     value = config._location-config;
   };
-in
-cfg:
-mkIf cfg.enable {
 
+  baseLocationConfig = {
+    proxyPass = cfg.host + ":" + (toString cfg.port);
+    proxyWebsockets = true;
+    recommendedProxySettings = true;
+    extraConfig = ''
+      client_max_body_size ${toString cfg.maxUpload};
+      proxy_read_timeout ${toString cfg.timeout};
+      proxy_send_timeout ${toString cfg.timeout};
+      send_timeout ${toString cfg.timeout};
+    '';
+  };
+in
+mkIf cfg.enable {
   enableACME = cfg.enableHTTPS != false;
 
   # these two options are mutually exclusive:
@@ -16,19 +27,8 @@ mkIf cfg.enable {
   addSSL = cfg.enableHTTPS == "both";
   forceSSL = cfg.enableHTTPS == true;
 
-  locations = {
-    "/" = mkIf (cfg.port != null) {
-      proxyPass = cfg.host + ":" + (toString cfg.port);
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
-      extraConfig = ''
-        client_max_body_size ${toString cfg.maxUpload};
-        proxy_read_timeout ${toString cfg.timeout};
-        proxy_send_timeout ${toString cfg.timeout};
-        send_timeout ${toString cfg.timeout};
-        ${cfg.extraConfig}
-      '';
-    };
-  }
-  // mapAttrs' mkLinkLocation cfg.links;
+  locations = lib.mkMerge [
+    { "/" = mkIf (cfg.port != null) baseLocationConfig; }
+    (mapAttrs' mkLinkLocation cfg.links)
+  ];
 }
