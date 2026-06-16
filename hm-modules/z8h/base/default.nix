bb4e5dee70eade1158e9fe9792e68535c840c6c3
blokyk <courvoisier.zoe@gmail.com>
2026-06-16 15:55:18 +0200

    z8h: move zle-prelude to zsh.localVariables

--- hm-modules/z8h/base/default.nix
+++ hm-modules/z8h/base/default.nix
@@ -1,7 +1,24 @@
-{
+{ ... }: {
   imports = [
     ./autosuggestions.nix
     ./completions.nix
     ./keybindings.nix
   ];
+
+  programs.zsh.localVariables = {
+    # mark the missing \n at the end of a command output with a red block
+    PROMPT_EOL_MARK = "%K{red} %k";
+
+    # only alphanums make up words in word-based zle widgets
+    WORDCHARS= "''";
+
+    # don't eat space when typing '|' after a tab completion
+    ZLE_REMOVE_SUFFIX_CHARS = "''";
+
+    # wait for 200ms for the continuation of a key sequence
+    KEYTIMEOUT = 20;
+
+    # disable highlighting of text pasted into the command line
+    zle_highlight = ["paste:none"];
+  };
 }
--- hm-modules/z8h/default.nix
+++ hm-modules/z8h/default.nix
@@ -28,17 +28,6 @@ in {
         mkdir -p ''${XDG_CACHE_HOME:=$HOME/.cache}/z4h
       '';
 
-      zle-prelude = lib.hm.dag.entryAfter [ "z4h-prelude" ] ''
-        PROMPT_EOL_MARK='%K{red} %k'    # mark the missing \n at the end of a command output with a red block
-        WORDCHARS='''                   # only alphanums make up words in word-based zle widgets
-        ZLE_REMOVE_SUFFIX_CHARS='''     # don't eat space when typing '|' after a tab completion
-        KEYTIMEOUT=20                   # wait for 200ms for the continuation of a key sequence
-        zle_highlight=('paste:none')    # disable highlighting of text pasted into the command line
-
-        autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
-        autoload -Uz run-help ''${^fpath}/run-help-^*.zwc(N:t)
-      '';
-
       z4h-init = lib.hm.dag.entryAfter [ "z4h-prelude" ] ''
         # todo: this is basically going to be a simplified version of main.zsh
         # without all the checks and installation stuff
@@ -65,6 +54,9 @@ in {
             unset _z4h_tty_fd
           fi
         fi
+
+        autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+        autoload -Uz run-help ''${^fpath}/run-help-^*.zwc(N:t)
       '';
     };
   };
